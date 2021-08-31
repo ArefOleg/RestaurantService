dfcd1465b29fdc4650ff18b73e36b9e37638c881
ArefOleg <serd-075@mail.ru>
2021-08-31 22:36:34 +0300

    Rework menu

---
+++ src/main/java/com/example/rest_demo/util/MenuUtil.java
@@ -0,0 +1,12 @@
+package com.example.rest_demo.util;
+
+import com.example.rest_demo.model.Menu;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MenuUtil {
+
+    private MenuUtil(){
+    }
+
+}
